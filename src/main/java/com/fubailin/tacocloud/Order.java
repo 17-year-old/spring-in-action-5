eecd1b5506612a45d63e4b41568531f50aab6418
fbl <fubail@qq>
2018-10-29 09:09:44 +0800

    添加其他视图

--- src/main/java/com/fubailin/tacocloud/Order.java
+++ src/main/java/com/fubailin/tacocloud/Order.java
@@ -1,16 +1,31 @@
 package com.fubailin.tacocloud;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 
 @Data
 public class Order {
+    @NotBlank(message = "不允许为空")
     private String name;
+    @NotBlank(message = "不允许为空")
     private String street;
+    @NotBlank(message = "不允许为空")
     private String city;
+    @NotBlank(message = "不允许为空")
     private String state;
+    @NotBlank(message = "不允许为空")
     private String zip;
+    @CreditCardNumber(message = "不是合法的信用卡号")
     private String ccNumber;
+    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
+            message="必须按照格式'月/年'输入,月和年都是两位数字")
     private String ccExpiration;
+    @Digits(integer=3, fraction=0, message="不是合法的CVV")
     private String ccCVV;
 }
 
--- src/main/java/com/fubailin/tacocloud/Taco.java
+++ src/main/java/com/fubailin/tacocloud/Taco.java
@@ -10,12 +10,12 @@ import java.util.List;
 public class Taco {
 
     // end::allButValidation[]
-    @NotNull
-    @Size(min=5, message="Name must be at least 5 characters long")
+    @NotNull(message = "不能为空")
+    @Size(min=5, message="至少输入5个字符")
     // tag::allButValidation[]
     private String name;
     // end::allButValidation[]
-    @Size(min=1, message="You must choose at least 1 ingredient")
+    @Size(min=1, message="需要选择至少一种配料")
     // tag::allButValidation[]
     private List<String> ingredients;
 
