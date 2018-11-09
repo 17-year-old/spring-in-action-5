0e6a0c3e16f644f1d20307421eede30fffb1052b
fbl <fubail@qq>
2018-11-09 17:12:53 +0800

    添加其他视图

--- src/main/java/com/fubailin/tacocloud/Taco.java
+++ src/main/java/com/fubailin/tacocloud/Taco.java
@@ -4,12 +4,13 @@ import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.Date;
 import java.util.List;
 
 @Data
 public class Taco {
 
-    // end::allButValidation[]
+    private long id;
     @NotNull(message = "不能为空")
     @Size(min=5, message="至少输入5个字符")
     // tag::allButValidation[]
@@ -17,6 +18,8 @@ public class Taco {
     // end::allButValidation[]
     @Size(min=1, message="需要选择至少一种配料")
     // tag::allButValidation[]
-    private List<String> ingredients;
+    private List<Ingredient> ingredients;
+    private Date createdAt;
+    private Date modifiedAt;
 
 }
